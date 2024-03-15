bb0811ed945fc6b99862199ae9c99afa5c96a532
Inlinera <ilkvakovv@gmail.com>
2024-03-15 23:27:07 +0500

    remade first version

--- js/index.js
+++ js/index.js
@@ -22,32 +22,44 @@ contentBlocks.classList.toggle('active');
 span.classList.toggle('active');
 span1.classList.toggle('active');
 };
-//
-const dialog = document.getElementById('myDialog');
-const dialogOpener = document.querySelector('.openDialogBtn');
-const dialogCloser = document.querySelector('.closeDialogBtn');
-//
-function closeOnBackDropClick({ currentTarget, target }) {
-  const dialog = currentTarget
-  const isClickedOnBackDrop = target === dialog
-  if (isClickedOnBackDrop) {
-    dialog.close()
-  }
+// MY POPUP
+function openModal1() {
+  document.querySelector('.modal1').classList.add('open');
+};
+//close бест модал виндов
+function closeModal1() {
+  document.querySelector('.modal1').classList.remove('open');
 }
-function openModalAndLockScroll() {
-  dialog.showModal()
-  document.body.classList.add('scroll-lock')
-}
-function returnScroll() {
-  document.body.classList.remove('scroll-lock')
-}
-function close() {
-  dialog.close()
-  returnScroll()
-}
-dialog.addEventListener('click', closeOnBackDropClick)
-dialogOpener.addEventListener('click', openModalAndLockScroll)
-dialogCloser.addEventListener('click', (event) => {
-  event.stopPropagation()
-  close()
+// Закрытие при нажатии escape(стрелочная функция)
+window.addEventListener('keydown', (e) => {
+document.querySelector('.modal1').classList.remove('open');
 });
+//close модал виндов при клике вне его
+document.querySelector('.modal1 .modal__info1').addEventListener('click', event => {
+ event._isClickWithInModal = true;
+});
+document.querySelector('.modal1').addEventListener('click', event => {
+ if (event._isClickWithInModal) return;
+ event.currentTarget.classList.remove('open');
+});
+//open бест модал виндов 2
+function openModal() {
+  document.querySelector('.modal').classList.add('open');
+};
+//close бест модал виндов 2
+function closeModal() {
+  document.querySelector('.modal').classList.remove('open');
+}
+// Закрытие при нажатии escape(стрелочная функция) 2
+window.addEventListener('keydown', (e) => {
+document.querySelector('.modal').classList.remove('open');
+});
+//close модал виндов при клике вне его 2
+document.querySelector('.modal .modal__info').addEventListener('click', event => {
+ event._isClickWithInBestModal = true;
+});
+document.querySelector('.modal').addEventListener('click', event => {
+ if (event._isClickWithInBestModal) return;
+ event.currentTarget.classList.remove('open');
+});
+//best modal window 
